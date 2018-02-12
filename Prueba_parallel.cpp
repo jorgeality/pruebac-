506113767c8e669844a5d6083b4192a7255fa2d0
jorgeality <jorgeality29@gmail.com>
2018-02-12 10:02:56 -0500

    ejemplo completo de lectura de archivos y contar las letras que se repiten en el archivo leido.

---
+++ Prueba_parallel.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+//lectura de archivos
+#include <fstream>
+//para usar la listas
+#include <list>
+//para usar los aceentos.
+#include <locale.h>
+
+
+
+using namespace std;
+
+int main() {
+	//para poder usar los acentos.
+  setlocale(LC_ALL, "");
+  list<int> letras;
+  ifstream in("quijote.txt");
+  string s, line;
+  int cont = 0; 
+  char caracter[] = {'a','á','A','Á','b','B','c','C','d','D','e','é','E','É','f','F','g','G','h','H','i','í','I','Í','j','J','k','K','l','L','m','M','n','N','ñ','Ñ','o','ó','O','Ó','p','P','q','Q','r','R','s','S','T','t','u','ú','v','V','w','W','x','X','y','Y','z','Z',',',';','.',':','-','_','"','´','!'};
+  while(getline(in, line))
+    s += line;
+    
+	cout<<s<<endl;
+    int num = sizeof caracter;
+   	
+    for(int i = 0; i < num;i++)
+    {
+  		for(int j = 0; j < s.size();j++)
+		{
+		   if(caracter[i] == s[j])
+		   {
+		   	cont++;
+		   }
+		}
+		if(cont > 0)
+		{
+			cout<<caracter[i]<<" = "<<cont<<endl;
+		}
+		
+		cont = 0;
+	}
+	
+  
+} 
+

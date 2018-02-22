ca67c4c74bedb7169c5ae5f0af929010f7cf790f
jorgeality <jorgeality29@gmail.com>
2018-02-21 20:22:43 -0600

    serial funcionando

---
+++ Prueba_serial.cpp
@@ -0,0 +1,115 @@
+﻿#include <string>
+
+#include <iostream>
+
+//lectura de archivos
+
+#include <fstream>
+
+//para usar los aceentos.
+
+#include <locale.h>
+
+
+
+using namespace std;
+
+
+
+
+int main() {
+  
+	//Usamos el setlocale, para poder usar los acentos.
+  
+	setlocale(LC_ALL, "");
+ 
+	/*En esta parte usamos el ifstream para leer el archivo*/
+  
+	ifstream in("quijotet.txt");
+ 
+	/*Declaracion de variables y arrays a usar*/
+  
+	string s, line;
+  
+	int cont = 0;
+  
+	char caracter[] = {'a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','T','t','u','v','V','w','W','x','X','y','Y','z','Z',',',';','.',':','-','_','"','!',' '};
+
+	/*En esta parte lo que hacemos es leer el documento linea por linea.
+
+	  Lo almacenamos todo en un sola variable de tipo string llamada
+ 's'. 
+	  Posteriormente cerramos el documento que se ha abierto para ser leido.*/
+  
+	while(getline(in, line))
+
+	    s += line;
+
+        in.close();
+    
+	 //Usamos el tamaño del  array caracter para su iteración en el ciclo for
+    
+     // en donde se ejecutara el proceso a realizar.
+    
+	int num = sizeof caracter;
+   
+	 
+
+
+	     /*
+	
+		Justo en esta parte podemos ver claramente como cada caracter del 
+
+		array est siendo comparada con cada uno de los caracteres del archivo.
+	
+	Usamos un for doble estilo matriz para poder comparar cada uno delos caracteres del arreglo con los del archivo.
+	     */
+	
+	
+	
+	for(int i = 0; i < num;i++)
+    	
+	{
+	
+
+  	for(int j = 0; j < s.size();j++)
+		{
+
+			//Comparaciones mencionadas.
+
+		   	if(caracter[i] == s[j])
+
+		   	{
+
+			//Contador de caracteres.
+
+		   		cont++;
+
+		   	}
+			
+		}
+			
+		/*En esta condicional solo decimos que si el contador que acaba de salir es mayor a cero 
+			 
+		entonces me imprima el caracter comparado, y el numero de veces que se repite.
+*/
+			
+		if(cont > 0)
+			
+		{
+				
+			cout<<caracter[i]<<" = "<<cont<<endl;
+			
+		}
+			
+		//Hacemos cero nuevamete el contador y "finalizamos el ciclo for".
+			
+		cont = 0;
+    	
+	}
+
+ 
+	return 0;
+
+}
+
